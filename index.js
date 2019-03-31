3d6d9fea53542638a3d0e545f336e3868beb326f
Eszter Horvath <horvath.eszter.h@gmail.com>
2019-03-31 19:05:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-1 + 80 // 81
-60 - 40 // 20
+function add(a, b){
+  return a+b
+}
+
+function subtruct(a,b) {
+  return a-b
+}
+
+function multiply(a,b){
+  multiply a*b
+}
+
+function divide (a,b){
+  divide a/b
+}
 2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
 5.0 / 2.5 // 2
 
